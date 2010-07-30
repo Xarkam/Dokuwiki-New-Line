1a4f4878edb490955bfea239f58dfb3dafbb02fc
Xarkam <xarkam@gmail.com>
2010-07-30 16:21:24 +0200

    Change licence information

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -3,14 +3,11 @@
  * French language file
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Raphael Wils <info@r-wils.fr>
- * @author     Bellière Ludovic <xrogaan@gmail.com>
- * @author     Benjamin HKB <http://benjamin.hkb.free.fr>
- * @author     iDo <iLoveiDo@gmail.com>
+ * @author     Xarkam <xarkam@gmail.com>
  */
 
 // settings must be present and set appropriately for the language
 $lang['encoding']   = 'utf-8';
 $lang['direction']  = 'ltr';
 
-$lang['qb_newlinebutton'] = 'Insérer le code de nouvelle ligne \\';
+$lang['qb_newlinebutton'] = 'Insérer le code de nouvelle ligne';
