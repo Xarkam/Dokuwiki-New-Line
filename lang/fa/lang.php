f1984310eae3f093381d66bbbaac7f61112285cd
Sam01 <m.sajad079@gmail.com>
2016-08-23 07:20:33 +0200

    translation update

---
+++ lang/fa/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Sam01 <m.sajad079@gmail.com>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'لیتر';
+$lang['qb_newlinebutton']      = 'قرار دادن کد خط جدید';
