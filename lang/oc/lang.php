6a7e7933fb0673dae725f0ab3ace5852caae901d
En Matt <heartattack@free.fr>
2019-02-28 15:10:46 +0100

    translation update

---
+++ lang/oc/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author En Matt <heartattack@free.fr>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['qb_newlinebutton']      = 'Inserir una nòva linha de còdi';
