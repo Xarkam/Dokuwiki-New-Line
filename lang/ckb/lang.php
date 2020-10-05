3fcee79b62db4d6695d1f0b6cfe8f0b6d6aa9be5
qezwan <qezwan@gmail.com>
2020-10-06 00:05:35 +0200

    translation update

---
+++ lang/ckb/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author qezwan <qezwan@gmail.com>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'rtl';
+$lang['qb_newlinebutton']      = 'دانانی کۆدی هێڵی نوێ';
