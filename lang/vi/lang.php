40013d559a04816fd40d155e320da191c029007b
Minh <phandinhminh@protonmail.ch>
2023-01-24 22:40:11 +0100

    translation update

---
+++ lang/vi/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Minh <phandinhminh@protonmail.ch>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['qb_newlinebutton']      = 'Chèn thêm dòng code mới';
